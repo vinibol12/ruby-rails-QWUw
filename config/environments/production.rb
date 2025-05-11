22728e8780dd309175380d3dbdf2e354cecb3e25
vini <vinisem@gmail.com>
2025-05-11 15:17:31 +1200

    Allow host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "rails-production-41b4.up.railway.app"
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
